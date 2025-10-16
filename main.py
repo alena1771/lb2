8d62721708b00144a26143518751b5f6089c3e07
alena1771 <alenaantropova19@gmail.com>
2025-10-16 21:05:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ if __name__ == "__main__":
     git add .
 git commit -m "Initial commit: Добавила основную структуру проекта."
 git tag v0.1
+echo "# Добавляем дополнительные файлы" >> file.txt
+git add .
+git commit -m "Добавлен вспомогательный файл."
+git tag v0.2
+
